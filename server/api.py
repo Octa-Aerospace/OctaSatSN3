331804f48a6c514a619cf0d317c5e769079ce4f8
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2024-01-15 00:08:27 -0300

    fix(pandas): remove pandas

--- server/api.py
+++ server/api.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI, HTTPException
 from typing import List
-import pandas as pd
+import csv
 
 app = FastAPI()
 
 def read_csv(file_path):
     try:
-        return pd.read_csv(file_path)
+        with open(file_path, mode='r', encoding='utf-8') as file:
+            csv_reader = csv.DictReader(file)
+            data = list(csv_reader)
+            return data
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="CSV file not found")
 
 @app.get("/", response_model=List[dict])
 async def get_historical_data():
     data = read_csv("data.csv")
-    last_20_records = data.tail(20).to_dict(orient='records')
+    last_20_records = data[-20:]  # Get the last 20 records
     return last_20_records
 
 @app.get('*', include_in_schema=False)
