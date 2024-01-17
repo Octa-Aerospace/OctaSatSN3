0674829defd3b3800fac370a0d4209827e7af465
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2024-01-17 00:01:49 -0300

    fix(timestamp)

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+import csv
 from time import sleep
-#
+from datetime import datetime
 from modules.Buzzer import Buzzer
 from modules.Camera import Camera
 from modules.GY91 import GY91
 from modules.GPS import GPS
 from modules.BME280 import BME280
-# from modules.E32 import E32
+import os
 
 class OctaSat:
   def __init__(self):
     self.BUZZER_PIN = 12
     self.I2C_ADDRESS = 0x68
     self.GPS_PORT = '/dev/ttyAMA0'
+    self.data = {}
 
   def init(self):
     self.gy91 = GY91(self.I2C_ADDRESS)
@@ -24,21 +26,36 @@ class OctaSat:
     self.buzzer.init()
 
   def read_data(self):
-    accel = self.gy91.get_accel()
-    gyro = self.gy91.get_gyro()
-    mag = self.gy91.get_mag()
+    # accel = self.gy91.get_accel()
+    # gyro = self.gy91.get_gyro()
+    # mag = self.gy91.get_mag()
     latitude, longitude = self.gps.read_data()
     temperature, humidity, pressure, altitude = self.bme280.get_packed_data()
 
-    print(f"Accelerometer: {accel['x']} {accel['y']} {accel['z']}")
-    print(f"Gyroscope: {gyro['x']} {gyro['y']} {gyro['z']}")
-    print(f"Magnetometer: {mag['x']} {mag['y']} {mag['z']}")
-    print(f"Latitude: {latitude} | Longitude: {longitude} | Altitude: {altitude}")
-    print(f"Temperature: {temperature} | Humidity: {humidity} | Pressure: {pressure}")
-    print("\n")
+    self.data = {
+      # 'accelerometer': accel,
+      # 'gyroscope': gyro,
+      # 'magnetometer': mag,
+      'timestamp': datetime.now(),
+      'latitude': latitude,
+      'longitude': longitude,
+      'altitude': altitude,
+      'temperature': temperature,
+      'humidity': humidity,
+      'pressure': pressure
+    }
 
   def save_data(self):
-    pass
+    headers = ['timestamp', 'latitude', 'longitude', 'altitude', 'temperature', 'humidity', 'pressure']
+    rows = [list(self.data.values())]
+
+    if not os.path.exists('data.csv'):
+      rows.insert(0, headers)
+
+    with open('data.csv', 'a', newline='') as file:
+      writer = csv.writer(file)
+      writer.writerows(rows)
+
 
   def send_data(self):
     pass
@@ -50,8 +67,8 @@ if __name__ == "__main__":
   device = OctaSat()
   device.init()
 
-  for i in range(10):
+  for i in range(20):
     device.read_data()
-    sleep(1)
-
+    device.save_data()
+    sleep(0.5)
   device.kill()
