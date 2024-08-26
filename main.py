cbd73b25c4948b3b6297fa25f9d87e1b9f101caf
CxrlosKenobi <cxrlos@kenobi.dev>
2024-08-26 01:49:39 -0400

    removing also gps conflicting sensor

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class OctaSat:
     def __init__(self):
         self.BUZZER_PIN = 12
         self.I2C_ADDRESS = 0x68
-        self.GPS_PORT = '/dev/ttyAMA0'
+        # self.GPS_PORT = '/dev/ttyAMA0'
         self.BUTTON_GPIO = 5
         self.data = {}
         self.setup_button()
@@ -25,7 +25,7 @@ class OctaSat:
 
     def init(self):
         # self.gy91 = GY91(self.I2C_ADDRESS)
-        self.gps = GPS(port=self.GPS_PORT)
+        # self.gps = GPS(port=self.GPS_PORT)
         self.bme280 = BME280()
         self.lora = LoRa()
         # self.camera = Camera()
@@ -38,7 +38,7 @@ class OctaSat:
         # accel = self.gy91.get_accel()
         # gyro = self.gy91.get_gyro()
         # mag = self.gy91.get_mag()
-        latitude, longitude = self.gps.read_data()
+        # latitude, longitude = self.gps.read_data()
         temperature, humidity, pressure, altitude = self.bme280.get_packed_data()
         #print(f'Latitude: {latitude}\nLongitude: {longitude}\nTemperature: {temperature}\nHumidity: {humidity}\nPressure: {pressure}\nAltitude: {altitude}')
         #print('\n')
@@ -69,7 +69,7 @@ class OctaSat:
             writer.writerows(rows)
 
     def send_data(self):
-        latitude, longitude = self.gps.read_data()
+        # latitude, longitude = self.gps.read_data()
         temperature, humidity, pressure, altitude = self.bme280.get_packed_data()
         payload = f'Latitude: {latitude}\nLongitude: {longitude}\nTemperature: {temperature}\nHumidity: {humidity}\nPressure: {pressure}\nAltitude: {altitude}'
         self.lora.begin_packet_radio(payload)
@@ -87,7 +87,6 @@ if __name__ == "__main__":
         while True:
             if GPIO.wait_for_edge(device.BUTTON_GPIO, GPIO.RISING):
                 while True:
-
                     device.read_data()
                     device.save_data()
                     device.send_data()
