bb284132834cc412c8eb7319657e947cc07cf8a5
Matilde <matilde.gaete.tapia07@gmail.com>
2024-09-13 17:23:29 -1000

    Update main.py
    main.py version w/o gy.91, GPS, button function

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from time import sleep
 from datetime import datetime
 from modules.Buzzer import Buzzer
 # from modules.Camera import Camera
-from modules.GY91 import GY91
-from modules.GPS import GPS
+#from modules.GY91 import GY91
+#from modules.GPS import GPS
 from modules.BME280 import BME280
 from modules.LoRa.lora import LoRa
 import os
@@ -15,7 +15,7 @@ class OctaSat:
     def __init__(self):
         self.BUZZER_PIN = 12
         self.I2C_ADDRESS = 0x68
-        self.GPS_PORT = '/dev/ttyAMA0'
+        #self.GPS_PORT = '/dev/ttyAMA0'
         self.BUTTON_GPIO = 5
         self.data = {}
         self.setup_button()
@@ -24,8 +24,8 @@ class OctaSat:
         GPIO.setup(self.BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def init(self):
-        self.gy91 = GY91(self.I2C_ADDRESS)
-        self.gps = GPS(port=self.GPS_PORT)
+        #self.gy91 = GY91(self.I2C_ADDRESS)
+        #self.gps = GPS(port=self.GPS_PORT)
         self.bme280 = BME280()
         self.lora = LoRa()
         # self.camera = Camera()
@@ -38,7 +38,7 @@ class OctaSat:
         # accel = self.gy91.get_accel()
         # gyro = self.gy91.get_gyro()
         # mag = self.gy91.get_mag()
-        latitude, longitude = self.gps.read_data()
+        #latitude, longitude = self.gps.read_data()
         temperature, humidity, pressure, altitude = self.bme280.get_packed_data()
         #print(f'Latitude: {latitude}\nLongitude: {longitude}\nTemperature: {temperature}\nHumidity: {humidity}\nPressure: {pressure}\nAltitude: {altitude}')
         #print('\n')
@@ -48,8 +48,8 @@ class OctaSat:
             # 'gyroscope': gyro,
             # 'magnetometer': mag,
             'timestamp': datetime.now(),
-            'latitude': latitude,
-            'longitude': longitude,
+            #'latitude': latitude,
+            #'longitude': longitude,
             'altitude': altitude,
             'temperature': temperature,
             'humidity': humidity,
@@ -69,9 +69,10 @@ class OctaSat:
             writer.writerows(rows)
 
     def send_data(self):
-        latitude, longitude = self.gps.read_data()
+        #latitude, longitude = self.gps.read_data()
         temperature, humidity, pressure, altitude = self.bme280.get_packed_data()
-        payload = f'Latitude: {latitude}\nLongitude: {longitude}\nTemperature: {temperature}\nHumidity: {humidity}\nPressure: {pressure}\nAltitude: {altitude}'
+        #payload = f'Latitude: {latitude}\nLongitude: {longitude}\nTemperature: {temperature}\nHumidity: {humidity}\nPressure: {pressure}\nAltitude: {altitude}'
+        payload = f'Temperature: {temperature}\nHumidity: {humidity}\nPressure: {pressure}\nAltitude: {altitude}'
         self.lora.begin_packet_radio(payload)
 
 
@@ -84,15 +85,13 @@ if __name__ == "__main__":
     device.init()
 
     try:
+
         while True:
-            if GPIO.wait_for_edge(device.BUTTON_GPIO, GPIO.RISING):
-                while True:
-
-                    device.read_data()
-                    device.save_data()
-                    device.send_data()
-                    sleep(0.5)
-
+            device.read_data()
+            device.save_data()
+            device.send_data()
+            sleep(0.5)
+        
     except KeyboardInterrupt:
         print(f'\n[!] Process interrupted')
         device.kill()
